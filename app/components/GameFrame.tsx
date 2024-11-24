5ebf1c0eb47c4dbaa109f932d244e746328f8ec6
wangwei <w1220w@126.com>
2024-11-24 21:55:42 +0800

    feat: update frame turbowrap

--- app/components/GameFrame.tsx
+++ app/components/GameFrame.tsx
@@ -2,7 +2,7 @@ export default function GameFrame() {
   return (
     <div className="w-full aspect-[16/9] bg-black/10 rounded-lg overflow-hidden shadow-xl relative">
       <iframe
-        src="https://scratch.mit.edu/projects/1099928310/embed"
+        src="https://turbowarp.org/1099928310/embed"
         className="absolute inset-0 w-full h-full border-0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
